6659ce22dae0dc6fce9978d823045abc033235ba
Kirk Broadhurst <kirk@DESKTOP-C8FCS05.localdomain>
2018-10-02 20:02:46 -0500

    initial unit tests

--- test/__init__.py
+++ test/__init__.py
@@ -1,14 +1,22 @@
 import boto3
+import logging
+import os
 import pytest
 import time
 from urllib.parse import urlparse
 
 
-database = 'pythena-test'
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+logger.addHandler(console_handler)
+
+database = 'pythena_test'
 table = 'unquoted'
 
 bucket = 'pythena-int'
 path = 'unquoted'
+key = '{}/data'.format(path)
 
 s3 = boto3.resource('s3')
 
@@ -18,34 +26,48 @@ def small_csv():
     """
     Tests that we can select some data with the client
     """
+    cleanup()
+    print('starting setup')
+
     # put the file onto s3
-    key = '{}/data'.format(path)
-    s3.Bucket(bucket).upload_file('data/small_unquoted.csv', key)
+    datafile = os.path.join(os.path.split(os.path.abspath(__file__))[0],
+                            'data/small_unquoted.csv')
+    s3.Bucket(bucket).upload_file(datafile, key)
+    print('uploaded file')
 
     # create the table in Athena
     query = '''CREATE EXTERNAL TABLE `{db}`.`{table}`
         (`id` int, `date` date, `float` float, `message` string)
         ROW FORMAT DELIMITED
             FIELDS TERMINATED BY ','
-            ESCAPED BY '\\'
-            LINES TERMINATED BY '\n'
+            LINES TERMINATED BY '\\n'
         LOCATION 's3://{bucket}/{path}/'
         tblproperties ("skip.header.line.count"="1")'''.format(
             db=database, table=table, bucket=bucket, path=path)
 
     athena_query('create database if not exists {}'.format(database))
-    athena_query(query)
-    yield
+    print('created database')
 
-    athena_query('drop table `{}`.`{}`'.format(database, table))
+    athena_query(query)
+    print('created table')
+
+    yield
+    cleanup()
+
+
+def cleanup():
+    athena_query('drop database if exists {} cascade'.format(database))
+    print('dropped database')
+
     boto3.client('s3').delete_object(Bucket=bucket, Key=key)
+    print('deleted object')
 
 
 def athena_query(sql):
     """
     Send a query to Athena. Return the results as a string
     """
-
+    logger.debug('running query {}'.format(sql))
     result_config = {'OutputLocation': 's3://{}/outputlocation'.format(bucket),
                      'EncryptionConfiguration': { 'EncryptionOption': 'SSE_S3' }
                     }
@@ -55,14 +77,18 @@ def athena_query(sql):
                                           ResultConfiguration=result_config)
 
     query_id = result['QueryExecutionId']
+    logger.debug('query submitted with id {}'.format(query_id))
     execution = client.get_query_execution(QueryExecutionId=query_id)['QueryExecution']
 
-    while (execution['Status']['State'] != 'SUCCESS'):
-        time.sleep(1000)
+    while (execution['Status']['State'] not in ['SUCCEEDED', 'FAILED']):
+        logger.debug('status: {}'.format(execution['Status']['State']))
+        time.sleep(1)
         execution = client.get_query_execution(QueryExecutionId=query_id)['QueryExecution']
 
+    logger.debug('query completed')
     path = execution['ResultConfiguration']['OutputLocation']
     key = path[path.find(bucket) + len(bucket) + 1:]
 
+    logger.debug('results at {}'.format(path))
     results = s3.Object(bucket, key).get()['Body'].read().decode('utf-8')
     return results
--- test/test_select.py
+++ test/test_select.py
@@ -1,13 +1,41 @@
-from . import athena_query
+from . import small_csv, athena_query, database, table
 #import pythena
 
 
-def test_select():
+def test_show_tables(small_csv):
     """
     Tests that we can select some data with the client
     """
-    result = athena_query('select count(*) from `pythena-test`.unquoted')
+    query = 'show tables in {}'.format(database)
+    result = athena_query(query)
+    print(result)
+
+
+def test_show_databases(small_csv):
+    """
+    Tests that we can select some data with the client
+    """
+    query = 'show databases'
+    result = athena_query(query)
+    print(result)
+
+
+def test_select(small_csv):
+    """
+    Tests that we can select some data with the client
+    """
+    query = 'select * from {}.{}'.format(database, table)
+    result = athena_query(query)
     print(result)
     #client = pythena.Client()
     #result = client.sql('select top 100 * from mytable')
+
+
+def test_select_1():
+    """
+    Tests that we can select the value 1; i.e. no data dependency
+    """
+    query = 'select 1'
+    result = athena_query(query)
+    print(result)
 
