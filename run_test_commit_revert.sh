72c7f3fb15da36d5a1c3b5a7d7036ec2bcf46d4b
Kirk Broadhurst <kirk@DESKTOP-C8FCS05.localdomain>
2018-10-29 14:10:57 -0500

    $1

--- run_test_commit_revert.sh
+++ run_test_commit_revert.sh
@@ -1,3 +1,8 @@
+if [ -z "$1" ]; then
+    echo "No commit message - use \"\" if genuinely no commit message"
+    exit 1
+fi
+
 if python -m pytest; then
     git add .
     git commit --allow-empty-message -m '$1'
