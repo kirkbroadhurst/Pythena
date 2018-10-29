259f066fe88cc6e90bdf0f33f926bb8d90510fdc
Kirk Broadhurst <kirk@DESKTOP-C8FCS05.localdomain>
2018-10-29 14:02:23 -0500

    $1

--- test/test_wait.py
+++ test/test_wait.py
@@ -6,7 +6,13 @@ import pytest_mock
 from pythena import Client
 
 
-def test_wait():
+def test_wait(mocker):
     client = Client()
+    mocker.patch.object(client.client, 'get_query_execution')
+
+    response = {'QueryExecution': {'Status': {'State': 'SUCCEEDED'}}}
+
+    client.client.get_query_execution.return_value = response
+    print(client.client.get_query_execution())
     pass
 
